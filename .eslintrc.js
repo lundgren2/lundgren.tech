e917e015d335fb6a76f99e65b83ec384aa12c655
Tobias Lundgren <tobias@lundgrendesign.se>
2018-03-04 20:51:00 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["standard", "plugin:react/recommended"],
+  plugins: ["standard", "react", "promise"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "react/prop-types": 0,
+    "react/display-name": 0
+  }
+};
