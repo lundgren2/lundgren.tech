8b60732aa89030852b6a2e6044bc9f8580602593
Tobias Lundgren <tobias@lundgrendesign.se>
2018-03-05 02:22:02 +0100

    Playing with the Emotion stuff

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,50 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled, { css } from 'react-emotion'
+import typography from '../../utils/typography'
+
+const Navbar = styled('nav')`
+  background: hotpink;
+  height: 75px;
+  display: flex;
+  align-items: center;
+`
+const NavItem = styled(Link)`
+  text-decoration: none;
+  padding: 0 20px;
+  &.${(props) => props.activeClassName} {
+    color: paleturquoise;
+    text-decoration: underline;
+  }
+`
+
+// NavItem.defaultProps = {
+//   activeClassName: 'active'
+// }
 
 const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '1.45rem'
     }}
   >
+
+    <Navbar>
+      <NavItem to="/">Home</NavItem>
+      <NavItem to="/page-2">About</NavItem>
+    </Navbar>
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
+
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+
+        <Link>
           Gatsby
         </Link>
       </h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,55 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { injectGlobal } from 'emotion'
+import styled, { css } from 'react-emotion'
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const Wrapper = styled.section`
+  align-items: center;
+  background: #282a36;
+  display: flex;
+  flex-direction: column;
+  font-family: system-ui;
+  justify-content: center;
+  padding: 5rem;
+`
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: #ff79c6;
+  margin-bottom: 0.5em;
+  & a {
+    color: #8be9fd;
+  }
+`
+
+const Subtitle = styled.p`
+  color: #bd93f9;
+  & a {
+    color: inherit;
+  }
+`
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <Wrapper>
+    <Title>
+      Hello World, this is my first component styled with{` `}
+      <a href="https://emotion.sh/">emotion</a>!
+    </Title>
+    <Subtitle>
+      <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-emotion/">
+        gatsby-plugin-emotion docs
+      </a>
+    </Subtitle>
+    <Link to="/page-2">Hello</Link>
+  </Wrapper>
 )
 
 export default IndexPage
