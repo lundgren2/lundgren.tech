c2361cb6e3bc945c1aed3fbdfcdfcced0b2cc61f
Tobias Lundgren <tobias@lundgrendesign.se>
2018-03-08 23:19:03 +0100

    Refactor layout to use emotion

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,11 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { TypographyStyle } from 'react-typography'
+import styled from 'react-emotion'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import typography from '../utils/typography'
 
 import './index.css'
+
+const Wrapper = styled('div')`
+  margin: 0 auto;
+  min-height: 45vh;
+  padding: 0px, 1.0875rem 1.45rem;
+  width: 100%; 
+`
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -19,16 +28,10 @@ const TemplateWrapper = ({ children }) => (
     />
     <TypographyStyle typography={typography} />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}
-    >
+    <Wrapper>
       {children()}
-    </div>
+    </Wrapper>
+    <Footer />
   </div>
 )
 
