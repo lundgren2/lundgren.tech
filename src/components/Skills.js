f9de8803f029f2aab31a40c733c685327085255b
Tobias Lundgren <tobias@lundgren.design>
2019-03-30 02:16:07 +0100

    Add some content to homepage

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,41 +1,6 @@
 import React from 'react';
 import { keyframes } from '@emotion/core';
 
-export default ({ items, color }) => (
-  <div
-    css={{
-      display: `flex`,
-      textIndent: `8px`,
-      paddingBottom: `12px`,
-
-      '& span': {
-        animation: `${topToBottom} 12s linear infinite 0s`,
-        opacity: 0,
-        position: `absolute`,
-
-        ':nth-child(2)': {
-          animationDelay: `3s`,
-        },
-
-        ':nth-child(3)': {
-          animationDelay: `6s`,
-        },
-
-        ':nth-child(4)': {
-          animationDelay: `9s`,
-        },
-      },
-    }}
-  >
-    {items &&
-      items.map(item => (
-        <span key={item} css={{ color }}>
-          {item}
-        </span>
-      ))}
-  </div>
-);
-
 const topToBottom = keyframes({
   '0%': {
     opacity: 0,
@@ -63,3 +28,30 @@ const topToBottom = keyframes({
     opacity: 0,
   },
 });
+
+const style = {
+  animation: `${topToBottom} 12s linear infinite 0s`,
+  opacity: 0,
+  position: `absolute`,
+  display: 'inline-block',
+  ':nth-child(2)': {
+    animationDelay: `3s`,
+  },
+  ':nth-child(3)': {
+    animationDelay: `6s`,
+  },
+
+  ':nth-child(4)': {
+    animationDelay: `9s`,
+  },
+};
+export default ({ items, color }) => (
+  <>
+    {items &&
+      items.map(item => (
+        <span key={item} css={{ ...style, color }}>
+          {item}.
+        </span>
+      ))}
+  </>
+);
